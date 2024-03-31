e504b66d2e57226807625f554e2cdddde0fcad2a
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-03-31 22:33:23 +0300

    add cart component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Navigate, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Products from './pages/Products';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/Cart.js
@@ -0,0 +1,7 @@
+const Cart = () => {
+  return (
+    <div>Корзина</div>
+  )  
+}
+
+export default Cart;
